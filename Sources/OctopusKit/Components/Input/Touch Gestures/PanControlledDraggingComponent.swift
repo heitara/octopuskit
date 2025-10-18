abba9e1940a78471876ae53fdac964949c3c0835
Emil Atanasov <heitara@gmail.com>
2025-10-18 14:34:11 +0300

    fix: disable log

--- Sources/OctopusKit/Components/Input/Touch Gestures/PanControlledDraggingComponent.swift
+++ Sources/OctopusKit/Components/Input/Touch Gestures/PanControlledDraggingComponent.swift
@@ -88,8 +88,8 @@ public final class PanControlledDraggingComponent: OKComponent, RequiresUpdatesP
         // A scene itself is not really draggable, so...
         
         if node is SKScene {
-            OKLog.warnings.debug("\(📜("A PanControlledDraggingComponent cannot be added to the scene entity — Removing."))")
-            OKLog.tips.debug("\(📜("See CameraPanComponent."))")
+//            OKLog.warnings.debug("\(📜("A PanControlledDraggingComponent cannot be added to the scene entity — Removing."))")
+//            OKLog.tips.debug("\(📜("See CameraPanComponent."))")
             self.removeFromEntity()
         }
     }
